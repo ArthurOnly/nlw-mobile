f72e4d161bff0790f1c563a39c8f8393239923e5
ArthurOnly <arthurmedeirospaiva56@gmail.com>
2020-08-06 11:16:47 -0300

    Create pageHeader

--- src/screens/favorites/index.tsx
+++ src/screens/favorites/index.tsx
@@ -2,9 +2,14 @@ import React from "react"
 
 import styles from "./styles"
 import { View } from "react-native"
+import PageHeader from "../../components/pageHeader"
 
 function Favorites() {
-  return <View style={styles.container}></View>
+  return (
+    <View style={styles.container}>
+      <PageHeader title="Meus proffys favoritos" />
+    </View>
+  )
 }
 
 export default Favorites
--- src/screens/teacherList/index.tsx
+++ src/screens/teacherList/index.tsx
@@ -3,8 +3,14 @@ import React from "react"
 import styles from "./styles"
 import { View } from "react-native"
 
+import PageHeader from "../../components/pageHeader"
+
 function TeacherList() {
-  return <View style={styles.container}></View>
+  return (
+    <View style={styles.container}>
+      <PageHeader title="Proffys disponíveis" />
+    </View>
+  )
 }
 
 export default TeacherList
